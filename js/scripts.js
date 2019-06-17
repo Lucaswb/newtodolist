afd297982ff0ef678c8a884dbd6f3746beec853f
Lucas Blasdel and Saswati Patra <lucas@>
2019-06-17 16:54:48 -0700

    fixed highlighter

--- js/scripts.js
+++ js/scripts.js
@@ -1,124 +1,154 @@
-// Business Logic for AddressBook ---------
-function AddressBook() {
-  this.contacts = [],
+//Businnes logic for toDo list
+var newArray=[]
+
+function ToDoList() {
+  this.items = [],
   this.currentId = 0
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts.push(contact);
-}
-// this will attempt to update the contact in the address Book.
-AddressBook.prototype.updateContact = function(id) {
-  var updateFirst= prompt("please enter a new first name:")
-  var updateLast= prompt("please enter a new Last name:")
-  var updateNumber= prompt("please enter a new number:")
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-
-        this.contacts[i].firstName = updateFirst;
-        this.contacts[i].lastName = updateLast;
-        this.contacts[i].phoneNumber = updateNumber;
-      }
-    }
-  };
+ToDoList.prototype.addItem = function(item) {
+  item.id = this.assignID(),
+  this.items.push(item)
 }
 
-AddressBook.prototype.assignId = function() {
+ToDoList.prototype.assignID = function() {
   this.currentId += 1;
   return this.currentId;
 }
-
-AddressBook.prototype.findContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        return this.contacts[i];
-      }
-    }
-  };
-  return false;
-}
-
-AddressBook.prototype.deleteContact = function(id) {
-  for (var i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id == id) {
-        delete this.contacts[i];
+ToDoList.prototype.deleteItem = function(id) {
+  for (var i=0; i,this.items.length; i++) {
+    if (this.items[i]) {
+      if (this.items[i].id== id) {
+        delete this.items[i];
         return true;
       }
     }
   };
   return false;
 }
-function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
-    showContact(this.id);
-  });
-  $("#buttons").on("click", ".deleteButton", function() {
-    addressBook.deleteContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
-  });
 
-  $("#buttons").on("click", ".updateButton", function() {
-    addressBook.updateContact(this.id);
-    $("#show-contact").hide();
-    displayContactDetails(addressBook);
+// function(newlistHighlight) {
+//   var itemsList = $("ul#items");
+//   var htmlForItemInfo = "";
+//   newlistDisplay.items.forEach(function(item) {
+//
+//     htmlForItemInfo += "<li id=" + item.id + 'class="highlight">' + item.itemToDo+ ": " + "</li>";
+//
+//   });
+//   itemsList.html(htmlForItemInfo);
+//   console.log(itemsList)
+// }
+
+ToDoList.prototype.findItem = function(id) {
+  for (var i=0; i<this.items.length; i++) {
+    if (this.items[i].id == id){
+      return this.items[i];
+    }
+  };
+  return false;
+};
+ToDoList.prototype.highlightItem=function(id) {
+  for (var i=0; i<this.items.length; i++) {
+    if (this.items[i].id == id){
+      this.items[i].highlight = true;
+    }
+  };
+  return false;
+};
+//Business logic for items
+function Item(itemToDo, description) {
+    this.itemToDo = itemToDo,
+    this.description= description
+    this.highlight= false
+
+}
+var newlist = new ToDoList ();
+
+function displayItemDetails(newlistDisplay) {
+  var itemsList = $("ul#items");
+  var htmlForItemInfo = "";
+  newlistDisplay.items.forEach(function(item) {
+
+    if (item.highlight == true)
+    {
+      htmlForItemInfo += "<li class='highlight' id=" + item.id + ">" + item.itemToDo+ ": " + "</li>";
+
+
+    }else {
+      htmlForItemInfo += "<li id=" + item.id + ">" + item.itemToDo+ ": " + "</li>";
+    }
+
   });
+  itemsList.html(htmlForItemInfo);
+  // console.log(newArray)
+};
+function appendArray(item) {
+  var newItem = "<li id=" + item.id + ">" + item.itemToDo+ ": " + "</li>";
+  newArray.push(item)
+  console.log(newArray)
 };
 
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.phoneNumber = phoneNumber
-}
+function attachItemListeners() {
+  $("ul#items").on("click", "li", function() {
+    showItem(this.id);
+  });
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
-function showContact(contactId) {
-  var contact = addressBook.findContact(contactId);
-  $("#show-contact").show();
-  $(".first-name").html(contact.firstName);
-  $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber);
+  $("#buttons").on("click", ".deleteButton", function() {
+    newlist.deleteItem(this.id);
+    $("#show-items").hide();
+    displayItemDetails(newlist);
+  });
+
+  $("#buttons").on("click", ".highlightButton", function() {
+    newlist.highlightItem(this.id);
+    // var stringID = "#" + this.id
+    // console.log(stringID)
+    // $(stringID).addClass("highlight")
+    $("#show-items").hide()
+    displayItemDetails(newlist);
+  });
+};
+
+function showItem(itemId) {
+  var item = newlist.findItem(itemId);
+  $("#show-items").show();
+  $(".addItem").html(item.itemToDo);
+  $(".addDescription").html(item.description);
+
   var buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
-  buttons.append("<button class='updateButton' id=" +  + contact.id + ">update</button>");
+  buttons.append("<button class='deleteButton' id=" + item.id + ">Delete</button>");
+  buttons.append("<button class='highlightButton' id=" + item.id + ">Highlight</button>");
 }
 
-// User Interface Logic ---------
-var addressBook = new AddressBook();
 
-function displayContactDetails(addressBookToDisplay) {
-  var contactsList = $("ul#contacts");
-  var htmlForContactInfo = "";
-  addressBookToDisplay.contacts.forEach(function(contact) {
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
-  });
-  contactsList.html(htmlForContactInfo);
-};
+
+
+// User Interface Logic ---------
 
 $(document).ready(function() {
-  attachContactListeners();
-  $("form#new-contact").submit(function(event) {
+  attachItemListeners();
+  $("form").submit(function(event) {
     event.preventDefault();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedItem = $("input#addItem").val();
+    var inputtedDescription = $("input#addDescription").val();
+    $("input#addItem").val("");
+    $("input#addDescription").val("");
+    var newItem = new Item(inputtedItem, inputtedDescription);
+    newlist.addItem(newItem);
+    appendArray(newlist);
+    displayItemDetails(newlist);
 
-    // The next three lines are new:
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-phone-number").val("");
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-    addressBook.addContact(newContact);
-    displayContactDetails(addressBook);
   })
 })
+// var item1 = new Item("do homework")
+// var item2 = new Item("do dishes")
+// newlist.addItem(item1)
+// newlist.addItem(item2)
+//
+//
+// console.log(newlist.findItem(2))
+// newlist.deleteItem(2)
+// console.log(newlist.items)
